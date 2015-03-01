39b4b788155e9a2dded5dcee6277d1317fd3079e
Lex Nicolaes <lexnicolaes@Lexs-iMac.local>
2015-03-01 22:02:07 +0100

    Add UIKit include, needed for UI* types

--- ZoomTransition/ZoomTransitionProtocol.h
+++ ZoomTransition/ZoomTransitionProtocol.h
@@ -24,6 +24,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 typedef void(^ZoomAnimationBlock)(UIImageView * animatedSnapshot, UIView * sourceView, UIView * destinationView);
 
